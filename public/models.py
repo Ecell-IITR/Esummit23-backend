3939c41a9bd1e7177bfd08851ec0b4b170b97f47
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-10-09 19:00:43 +0530

    speaker model created

--- public/models.py
+++ public/models.py
@@ -1,3 +1,24 @@
+from datetime import date
 from django.db import models
 
+
+class Speakers(models.Model):
+    name = models.CharField(max_length=120)
+    designation = models.CharField(
+         verbose_name='Designation', max_length=1000, blank=True)
+    profile_image = models.ImageField(
+          upload_to='speakers/', verbose_name='Profile Image',  blank=True, null=True)
+    date = models.DateField()
+    time = models.TimeField()
+    def __str__(self):
+            return self.name
+
+    class Meta:
+            """
+            Meta class for Speaker
+            """
+            verbose_name_plural = "Speakers"
+
+    def __str__(self):
+            return self.name
 # Create your models here.
