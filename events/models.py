0d4d91b7fab63901c253a483624e44c89603bc1c
Ishika <ishikakumari1906@gmail.com>
2022-10-18 22:36:40 +0530

    events api

--- events/models.py
+++ events/models.py
@@ -11,6 +11,12 @@ class Team(models.Model):
         upload_to='Teams/', verbose_name='Team Image',  blank=True, null=True)
     team_mail = models.EmailField(
         verbose_name='Team Email', max_length = 254)
+    team_phone = models.CharField(
+        verbose_name='Team pnumber', max_length=15, blank=True)
+    team_twitter = models.CharField(
+        verbose_name='Team twitter', max_length=120, blank=True)
+    team_linkedin = models.CharField(
+        verbose_name='Team linkedin', max_length=120, blank=True)     
    
     # created = models.DateTimeField(default=timezone.now)
     # updated = models.DateTimeField(auto_now=True)
@@ -18,4 +24,3 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
-        
