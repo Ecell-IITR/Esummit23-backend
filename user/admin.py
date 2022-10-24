66efc1a49e16af77fad4328966bc5d390b4a62be
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-10-24 15:29:21 +0530

    proff and student created

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from user.models.role.startup import StartupUser
 from user.models.role.ca import CAUser
+from user.models.role.student import StudentUser
+from user.models.role.proff import ProffUser
 # Register your models here.
 admin.site.register(StartupUser)
 admin.site.register(CAUser)
+admin.site.register(StudentUser)
+admin.site.register(ProffUser)
--- user/models/role/startup.py
+++ user/models/role/startup.py
@@ -2,13 +2,16 @@ from user.models.abstarct import AbstractProfile
 from events.models import Services
 from django.db import models
 
+
 class StartupUser(AbstractProfile):
     startup_name = models.CharField(max_length=50, verbose_name="Startup Name")
-    domain=models.CharField(max_length=50, verbose_name="Domain")
-    category=models.CharField(max_length=50, verbose_name="Category")
-    services=models.ManyToManyField(Services, verbose_name="Services")
-    description=models.CharField(max_length=200, verbose_name="Description")
+    domain = models.CharField(max_length=50, verbose_name="Domain")
+    category = models.CharField(max_length=50, verbose_name="Category")
+    services = models.ManyToManyField(Services, verbose_name="Services")
+    description = models.CharField(max_length=200, verbose_name="Description")
     esummit_id = models.CharField(max_length=20, unique=True, db_index=True)
+    referred_by = models.CharField(max_length=20,null=True,blank=True,default="")
+
     def save(self, *args, **kwargs):
         professional_tag = "stp"
 
@@ -21,4 +24,3 @@ class StartupUser(AbstractProfile):
                 unique_value = 0
             self.esummit_id = 'ES23'+professional_tag + \
                 str((unique_value + 1) * 31)
-    
