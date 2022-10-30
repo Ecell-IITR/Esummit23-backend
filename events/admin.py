1e3dda27b909f3e84014e95c05e0c64a3ee1c0a2
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-10-30 15:39:37 +0530

    cordinator created

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import Services,EventCoordinator
 
+admin.site.register(Services)
+admin.site.register(EventCoordinator)
 
 # Register your models here.
--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 
+from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,17 @@ class Services(models.Model):
     desc=models.CharField(max_length=200)
     image=models.ImageField(upload_to='services/')
 
+class EventCoordinator(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Coordinator Name")
+    email = models.EmailField(max_length=100, verbose_name="Email Id")
+    phone_number = models.IntegerField(verbose_name="Phone Number")
+
+    class Meta:
+        """
+        Meta class for EventCoordinator
+        """
+        verbose_name_plural = 'Event Coordinators'
+
+
+    def __str__(self):
+        return self.name
