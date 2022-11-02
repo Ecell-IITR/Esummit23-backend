c516ddcc52f140d6eb4a87b5dd0fb6c299c0756e
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-11-03 00:49:31 +0530

    events admin pannel

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Services,EventCoordinator,EventsFAQ,EventsPartners,EventRounds,EventRules,Event
-
+from .models import EventSeo
 from django.core.mail import send_mail
 
 class AdminRound(admin.ModelAdmin):
@@ -26,14 +26,56 @@ class AdminRound(admin.ModelAdmin):
         #         send_mail('Subject here', 'Here is the message.', 'from@example.com',[i.email], fail_silently=False)
             
         
-    send_EMAIL.short_description = "Send an email to selected users"
+    
 # Register your models here.
 
+@admin.register(EventCoordinator)
+class CoordinatorList(admin.ModelAdmin):
+    list_display = ["name", "email", "phone_number"]
 
-admin.site.register(Services)
-admin.site.register(EventCoordinator)
+
+class EventFAQInlines(admin.TabularInline):
+    model = Event.event_faqs.through
+    verbose_name_plural = "FAQS"
+    extra = 2
+
+
+class EventCoordinatorInlines(admin.TabularInline):
+    model = Event.events_coordinators.through
+    verbose_name_plural = "Event Coordinators"
+    extra = 2
+
+class EventRuleInlines(admin.TabularInline):
+    model = Event.event_rules.through
+    verbose_name_plural = "Event Rules"
+    extra = 2
+
+
+
+
+class EventPartnerInlines(admin.TabularInline):
+    model = Event.event_partners.through
+    verbose_name_plural = "Event Partners / Sponsors"
+    extra = 3
+
+class EventRoundInlines(admin.TabularInline):
+    model = Event.event_rounds.through
+    verbose_name_plural = "Event Rounds"
+    extra = 2
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["event_name", "tagline", "event_priority"]
+    search_fields = ["event_name", "tagline", ]
+    list_filter = ["event_status", ]
+    inlines = [EventFAQInlines, EventCoordinatorInlines, EventRuleInlines,EventPartnerInlines,EventRoundInlines]
+    exclude = ['event_faqs','event_rules', 'events_coordinators', 'event_partners' ,'event_perks',
+               'event_rounds']
+   
+# admin.site.register(Services)
+# admin.site.register(EventCoordinator)
 admin.site.register(EventsFAQ)
 admin.site.register(EventsPartners)
 admin.site.register(EventRules)
 admin.site.register(EventRounds,AdminRound)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventSeo)
