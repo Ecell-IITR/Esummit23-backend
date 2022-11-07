b4c694f62a8aeac3f96b01d8920d855bee49642f
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-11-07 19:03:38 +0530

    Update models.py

--- CAP/models.py
+++ CAP/models.py
@@ -1,7 +1,11 @@
-from unicodedata import name
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=50, verbose_name="Task Name")
     # to be finish later
+class Resources(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Resource Name")
+    file = models.FileField(upload_to='capresources/')
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
