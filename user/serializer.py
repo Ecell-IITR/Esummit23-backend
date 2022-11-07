8183b310b90ff024262133318936b56c376ab41d
pranav <76247369+pranav-iitr@users.noreply.github.com>
2022-11-08 00:53:07 +0530

    bfixes

--- user/serializer.py
+++ user/serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from .models import querry
 
+from .models import querry 
 
-class QuerrySerializer(serializers.ModelSerializer):
+class QuerrySerializer(serializers.Serializer):
     class Meta:
-        model = querry
-        include=["name","email","phone_number","message"]
+        model = querry.Querry
+        execlude=["name","email","phone_number","message"]
+    def create(self,validated_data):
+            return querry.Querry.objects.create(**validated_data)
         
--- user/views.py
+++ user/views.py
@@ -6,19 +6,18 @@ from rest_framework import status
 class QuerryView(generics.GenericAPIView):
     serializer_class=QuerrySerializer
     def post(self,request):
-        try:
+        
             
-            data={"name":request.data["name"],"email":request.data["email"],"phone_number":request.data["phone_number"],"message":request.data["message"]  }
+            data={"name":request.data.get("name"),"email":request.data.get("email"),"phone_number":request.data.get("phone_number"),"message":request.data.get("message")  }
           
             db_entry=QuerrySerializer(data=data)
-            print(db_entry)
+            
             if db_entry.is_valid():
+                print(db_entry.is_valid())
                 db_entry.save()
-                return Response("Successful", status=status.HTTP_200_OK);
+                return Response( status=status.HTTP_201_CREATED);
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        except:
-            pass
+        
       
-        return Response("unsuccessful", status=status.HTTP_400_BAD_REQUEST);
-
+        
 # Create your views here.
