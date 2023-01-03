c818e3a3f07d81507bd3c3d477b5567bc51bdd1f
Pranav Arya <pranavleo22@gmail.com>
2023-01-03 22:37:07 +0530

    Update production.py

--- esummit/setting/production.py
+++ esummit/setting/production.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*","api.esummit.in",]
 
