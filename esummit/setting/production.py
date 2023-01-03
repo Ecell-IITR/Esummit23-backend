fd7f0179ab523bba455442ee0c95bd58d81a748e
Pranav Arya <pranavleo22@gmail.com>
2023-01-03 22:31:14 +0530

    Update production.py

--- esummit/setting/production.py
+++ esummit/setting/production.py
@@ -31,7 +31,7 @@ SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*","api.esummit.in",]
 
 
 # Application definition
