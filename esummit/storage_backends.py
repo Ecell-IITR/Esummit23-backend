1efbd7377cc57d8eedd103422a8849a867852c05
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-01-08 20:11:31 +0530

    bug fixed

--- esummit/storage_backends.py
+++ esummit/storage_backends.py
@@ -1,7 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
-    location = 'esuumit/media'
+    location = 'esummit/media'
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
