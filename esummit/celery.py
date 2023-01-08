a4cee563c527927dda35762dd9796f9f692d2a4f
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-01-08 18:54:30 +0530

    bug fiuxed

--- esummit/celery.py
+++ esummit/celery.py
@@ -4,8 +4,6 @@ from celery import Celery
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esummit.setting.production')
-
-
 app = Celery("esummit")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
