f1be89df4110712b63274066f0a0cff31f3dbef5
Pranav Arya <pranavleo22@gmail.com>
2023-01-22 09:09:33 +0530

    Update celery.py

--- esummit/celery.py
+++ esummit/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esummit.setting.devlopment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esummit.setting.production')
 app = Celery("esummit")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
