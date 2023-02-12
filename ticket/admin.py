f506704516fa0a55d47c2dd0022c4114c5f6ec38
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-02-12 19:07:35 +0530

    Ticket admin updated

--- ticket/admin.py
+++ ticket/admin.py
@@ -5,10 +5,14 @@ class Reffreal(admin.ModelAdmin):
     exclude = ( 'usage',)
     list_display = ('owner', 'code','usage')
 
+class TicketsAdmin(admin.ModelAdmin):
+    list_display = ('Person', 'name', 'quantity')
+
+    search_fields = ('Person__email', 'name')
 
 admin.site.register(Plan)
 admin.site.register(Payment)
-admin.site.register(Ticket)
+admin.site.register(Ticket,TicketsAdmin)
 admin.site.register(ReffealCode,Reffreal)
 
 
