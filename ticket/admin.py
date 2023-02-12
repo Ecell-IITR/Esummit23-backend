e6623527fa8ee84be26b938bd57a24ee8787678e
Ishika <ishikakumari1905@gmail.com>
2023-02-13 03:54:37 +0530

    mass mailing

--- ticket/admin.py
+++ ticket/admin.py
@@ -1,19 +1,34 @@
 from django.contrib import admin
-from .models import Plan, Payment, Ticket,ReffealCode
-
-class Reffreal(admin.ModelAdmin):
-    exclude = ( 'usage',)
-    list_display = ('owner', 'code','usage')
-
-class TicketsAdmin(admin.ModelAdmin):
-    list_display = ('Person', 'name', 'quantity')
-
-    search_fields = ('Person__email', 'name')
-
+from .models import Plan, Payment, Ticket
+from user.tasks import send_link_email_task
+from django.shortcuts import render
 admin.site.register(Plan)
 admin.site.register(Payment)
-admin.site.register(Ticket,TicketsAdmin)
-admin.site.register(ReffealCode,Reffreal)
 
 
+class ticketAdmin(admin.ModelAdmin):
+   exclude = ('created', 'updated')
+   actions=["sendMail"]
+
+
+   def sendMail(self, request, queryset):
+       print(request.POST)
+       if 'submit' in request.POST :
+         
+         
+         for query in queryset:
+            email_address = query.Person.email
+          
+            mail_message= request.POST['message']
+            attachment = request.POST['importData']
+            mail_subject = request.POST['Subject']
+            send_link_email_task.delay( email_address,mail_message,mail_subject,attachment
+                )
+       return render(request,
+                      'mail.html')
+        
+admin.site.register(Ticket, ticketAdmin)
+          
+
+        
 # Register your models here.
