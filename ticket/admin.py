c0901ea2370585304333cb6f6e5c3df51c4bbc78
pranav <76247369+pranav-iitr@users.noreply.github.com>
2023-02-14 01:55:14 +0530

    Update admin.py

--- ticket/admin.py
+++ ticket/admin.py
@@ -50,6 +50,37 @@ class TicketsAdmin(admin.ModelAdmin):
 
         return response
 
+class TicketsAdmin2(admin.ModelAdmin):
+    list_display = ('Person', 'name', 'quantity')
+    search_fields = ('Person__email', 'name')
+    actions = ['download_csv']
+    def download_csv(self, request, queryset):
+    
+
+
+        f = StringIO()
+        writer = csv.writer(f)
+        writer.writerow(["name", "email", "quanty", "phone"])
+
+        for querry in queryset:
+            phone=""
+            print(querry.Person.student.phone_number)
+            if querry.Person.student:
+                phone=querry.Person.student.phone_number
+            elif querry.Person.ca:
+                phone=querry.Person.ca.phone_number
+            elif querry.Person.proff:
+                phone=querry.Person.proff.phone_number
+            print(phone)
+            writer.writerow([querry.Person,querry.Person.email,querry.quantity,querry.Person.student.phone_number])
+
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
+        return response
+
+
+
 admin.site.register(Plan)
 admin.site.register(Payment)
 admin.site.register(Ticket, TicketsAdmin)
