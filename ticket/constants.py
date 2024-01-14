5f6d9dc037a3468b57a0f5f4a9ac9247691b888d
ishika <ishikakumari1906@gmail.com>
2024-01-14 22:55:08 +0530

    message changed

--- ticket/constants.py
+++ ticket/constants.py
@@ -7,7 +7,7 @@ class PaymentStatus:
 class Plans:
     Plans = { "SSP": {"price": 599, "description": "SSP Description","mail_templete":" whatsapp link for the group "}, "PP": {"price": 2499, "description": "Professional Pass Description","mail_templete":" whatsapp link for the group "}, 
              "SEPP": {"price": 9999, "description": "Startup Expo Professional Pass Description","mail_templete":" whatsapp link for the group "}, "PSP": {"price": 1799, "description": "Premium Student Pass Description","mail_templete":" whatsapp link for the group "}, 
-             "TTP": {"price": 1, "description": "Tech team Description","mail_templete":" whatsapp link for the group"}}
+             "TTP": {"price": 1, "description": "Tech team Description","mail_templete":" You've successfully enrolled in Emerge! Do join the WhatsApp group using the provided link for further updates and great networking experience. https://chat.whatsapp.com/Gi9DGhmU1mOEOoc4M1qCLv"}}
 
     def plan_quantity(self, cost):
         array = list(self.Plans.keys())
