3af4066c84cae0572e86b55b71069f898aeb2869
ishika <ishikakumari1906@gmail.com>
2024-01-19 00:51:46 +0530

    tickets added

--- ticket/constants.py
+++ ticket/constants.py
@@ -5,13 +5,27 @@ class PaymentStatus:
 
 
 class Plans:
-    Plans = { "SSP": {"price": 599, "description": "SSP Description","mail_template":"Venue: Campus, IIT Roorkee"}, 
+    Plans = { "SSP": {"price": 599, "description": "Standard Student Pass Description","mail_template":"Venue: Campus, IIT Roorkee"}, 
              "PP": {"price": 2499, "description": "Professional Pass Description","mail_template":"Venue: Campus, IIT Roorkee"}, 
              "SEPP": {"price": 9999, "description": "Startup Expo Professional Pass Description","mail_template":"Venue: Campus, IIT Roorkee"}, 
              "PSP": {"price": 1799, "description": "Premium Student Pass Description","mail_template":"Venue: Campus, IIT Roorkee"}, 
              "EBC":{"price":149,"description":"Emerge Boot Camp Description","mail_template":"You've successfully enrolled in Emerge! Do join the WhatsApp group using the provided link for further updates and great networking experience. https://chat.whatsapp.com/Gi9DGhmU1mOEOoc4M1qCLv"},
              "TTP": {"price": 1, "description": "Tech team Description","mail_template":" You've successfully enrolled in Emerge! Do join the WhatsApp group using the provided link for further updates and great networking experience. https://chat.whatsapp.com/Gi9DGhmU1mOEOoc4M1qCLv"},
-             "EDP":{"price":249,"description":"Eduquest Description","mail_template":"You've successfully enrolled in Eduquest! Do join the WhatsApp group using the provided link for further updates and great networking experience.https://chat.whatsapp.com/FWEoMFGCzbTHkZNQsNu6uj "}}
+             "EDP":{"price":249,"description":"Eduquest Description","mail_template":"You've successfully enrolled in Eduquest! Do join the WhatsApp group using the provided link for further updates and great networking experience.https://chat.whatsapp.com/FWEoMFGCzbTHkZNQsNu6uj "},
+             "MUND":{"price":1699,"description":"MUN Discount Page","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SSPDF":{"price":599,"description":"Standard Student Pass Description First","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SSPDS":{"price":499,"description":"Standard Student Pass Description Second","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SSPDT":{"price":389,"description":"Standard Student Pass Description Third","mail_template":"Venue: Campus, IIT Roorkee"},
+             "PPPDF":{"price":2499,"description":"Professional Pass Description First","mail_template":"Venue: Campus, IIT Roorkee"},
+             "PPPDS":{"price":2269,"description":"Professional Pass Description Second","mail_template":"Venue: Campus, IIT Roorkee"},
+             "PSPDF":{"price":1799,"description":"Premium Student Pass Discounted First","mail_template":"Venue: Campus, IIT Roorkee"},
+             "PSPDS":{"price":1669,"description":"Premium Student Pass Discounted Second","mail_template":"Venue: Campus, IIT Roorkee"},
+             "PSPDT":{"price":1499,"description":"Premium Student Pass Discounted Third","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SEPPDF": {"price": 8999, "description": "Startup Expo Professional Pass Discounted First","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SEPPDS": {"price": 7949, "description": "Startup Expo Professional Pass Discounted Second","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SEPPDT": {"price": 6959, "description": "Startup Expo Professional Pass Discounted Third","mail_template":"Venue: Campus, IIT Roorkee"},
+             "SEPPDF": {"price": 5939, "description": "Startup Expo Professional Pass Discounted Fourth","mail_template":"Venue: Campus, IIT Roorkee"},
+             }
 
     def plan_quantity(self, cost):
         array = list(self.Plans.keys())
