e6eb59a8e039fc3678aa79b5625c86f884cf2233
ishika <ishikakumari1906@gmail.com>
2024-01-26 16:13:06 +0530

    get person data

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import OtpSendNew, OtpVerifyNew ,  QuerryView, LoginApiView,getfile, SignupView, OtpView, TeamSignupView, UserServices, VerifyView, NewTeamSignupView,OtpSignupView, OTPSignupVerify ,send_purchase_confirmation,TeamecellOtpView,TeamecellVerifyView,getabstract
+from .views import OtpSendNew, OtpVerifyNew ,  QuerryView, LoginApiView,getfile, SignupView, OtpView, TeamSignupView, UserServices, VerifyView, NewTeamSignupView,OtpSignupView, OTPSignupVerify ,send_purchase_confirmation,TeamecellOtpView,TeamecellVerifyView,getperson
 
 urlpatterns = [
     path('login', LoginApiView.as_view(), name='Loginview'),
     path('getfile',getfile),
-    path('getperson',getabstract),
+    path('getperson',getperson),
     # path('getproff',getproff),
     # path('getca',getca),
     # path('getstartup',getstartup),
